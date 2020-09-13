5d6cdce7e0a228bd068e00ae5d29cb09dd342a38
willhughes11 <willhughes2110@gmail.com>
2020-09-13 13:44:33 -0400

    1st commit

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    
+    @IBAction func incrementCount(){
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
+    
+    @IBAction func decrementCount(){
+        self.count -= 1
+        self.label.text = "\(self.count)"
     }
 
 
